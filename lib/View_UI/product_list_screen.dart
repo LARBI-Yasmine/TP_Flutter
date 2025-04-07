a162009efa4516687a4bcb0ca1ac66f0e2866e5c
l3miage-larbiy <larbi1992yasmine@gmail.com>
2025-04-07 16:59:06 +0200

    add file product_list__screen.dart

---
+++ lib/View_UI/product_list_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../View_Model/product_view_model.dart';
+import 'product_detail_screen.dart';
+
+class ProductListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Produits")),
+      body: Consumer<ProductViewModel>(
+        builder: (context, viewModel, child) {
+          if (viewModel.loading) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          return ListView.builder(
+            itemCount: viewModel.products.length,
+            itemBuilder: (context, index) {
+              final product = viewModel.products[index];
+              return ListTile(
+                title: Text(product.title),
+                subtitle: Text("\$${product.price.toStringAsFixed(2)}"),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => ProductDetailScreen(product: product),
+                    ),
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
